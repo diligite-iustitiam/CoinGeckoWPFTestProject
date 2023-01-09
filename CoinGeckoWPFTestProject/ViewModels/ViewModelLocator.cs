aa8256703e5ec70051adaad2cf60f4c40654a8c4
diligite-iustitiam <107812456+diligite-iustitiam@users.noreply.github.com>
2023-01-09 21:54:22 +0200

    ViewModelLocator logic implemented, data context in MainWindow changed to MainWindowModel

--- CoinGeckoWPFTestProject/ViewModels/ViewModelLocator.cs
+++ CoinGeckoWPFTestProject/ViewModels/ViewModelLocator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace CoinGeckoWPFTestProject.ViewModels
 {
     internal class ViewModelLocator
     {
+        public MainWindowViewModel MainWindowModel => App.Host.Services.GetRequiredService<MainWindowViewModel>();
     }
 }
