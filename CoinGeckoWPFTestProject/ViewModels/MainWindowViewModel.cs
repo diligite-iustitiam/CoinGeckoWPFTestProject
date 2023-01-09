6283cd7f08dcf6eff7a9bb624baa25d4c30c4200
diligite-iustitiam <107812456+diligite-iustitiam@users.noreply.github.com>
2023-01-09 21:34:06 +0200

    Implemented MainWindow and BaseViewModel

---
+++ CoinGeckoWPFTestProject/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using CoinGeckoWPFTestProject.Infrastructure.Commands;
+using CoinGeckoWPFTestProject.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CoinGeckoWPFTestProject.ViewModels
+{
+    internal class MainWindowViewModel : BaseViewModel
+    {
+        #region ChangeTabIndexCommand
+
+        private int _SelectedPageIndex = 1;
+
+        public int SelectedPageIndex { get => _SelectedPageIndex; set => Set(ref _SelectedPageIndex, value); }
+
+
+
+        public ICommand ChangeTabIndexCommand { get; }
+
+        private bool CanChangeTabIndexCommandExecute(object p) => _SelectedPageIndex >= 0;
+
+        private void OnChangeTabIndexCommandExecuted(object p)
+        {
+            if (p is null) return;
+            SelectedPageIndex += Convert.ToInt32(p);
+        }
+        #endregion
+        #region Title
+        private string _Title = "CoinGeckoWPFTestProject";
+        /// <summary>Window title</summary>
+        public string Title
+        {
+            get { return _Title; }
+            set => Set(ref _Title, value);
+        }
+        #endregion
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+
+        public MainWindowViewModel()
+        {
+            #region Commands           
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+            ChangeTabIndexCommand = new LambdaCommand(OnChangeTabIndexCommandExecuted, CanChangeTabIndexCommandExecute);
+            #endregion
+        }
+
+    }
+}
