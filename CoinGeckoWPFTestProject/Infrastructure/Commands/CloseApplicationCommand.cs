0c975b5a8813bf4f89f7a79f81be58ff1602d348
diligite-iustitiam <107812456+diligite-iustitiam@users.noreply.github.com>
2023-01-09 21:18:20 +0200

    Base commands created

---
+++ CoinGeckoWPFTestProject/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,13 @@
+﻿using CoinGeckoWPFTestProject.Infrastructure.Commands.Base;
+using System;
+using System.Windows;
+
+namespace CoinGeckoWPFTestProject.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+        public override void Execute(object? parameter) => Application.Current.Shutdown();
+        
+    }
+}
