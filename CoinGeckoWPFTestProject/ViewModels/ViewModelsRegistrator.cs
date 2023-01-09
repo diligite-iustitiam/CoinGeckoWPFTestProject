1b8061281e87f203daca062819f5414f18cd1509
diligite-iustitiam <107812456+diligite-iustitiam@users.noreply.github.com>
2023-01-09 21:40:44 +0200

    ServiceRegistrator and ViewModelsRegistrator

---
+++ CoinGeckoWPFTestProject/ViewModels/ViewModelsRegistrator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinGeckoWPFTestProject.ViewModels
+{
+    internal static class ViewModelsRegistrator
+    {
+        public static IServiceCollection RegisterViewModels(this IServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
+            return services;
+        }
+    }
+}
