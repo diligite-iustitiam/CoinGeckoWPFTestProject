21526ec0615b99bc5fbca86a8dac1ff2063c09ef
diligite-iustitiam <107812456+diligite-iustitiam@users.noreply.github.com>
2023-01-09 21:46:39 +0200

    Startup project moved into Program, App changed to work with host, host logic implemented

--- CoinGeckoWPFTestProject/App.xaml.cs
+++ CoinGeckoWPFTestProject/App.xaml.cs
@@ -1,17 +1,54 @@
-﻿using System;
+﻿using CoinGeckoWPFTestProject.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace CoinGeckoWPFTestProject
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+{   
     public partial class App : Application
     {
+        public static bool IsDesignMode { get; private set; } = true;
+
+        private static IHost __Host;
+
+        public static IHost Host => __Host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            IsDesignMode = false;
+            var host = Host;
+            base.OnStartup(e);
+
+            await host.StartAsync().ConfigureAwait(false);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            var host = Host;
+            await host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+            __Host = null;
+        }
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.RegisterViewModels();
+        }
+
+        public static string CurrentDirectory => IsDesignMode
+            ? Path.GetDirectoryName(GetSourceCodePath())
+            : Environment.CurrentDirectory;
+
+        private static string GetSourceCodePath([CallerFilePath] string Path = null) => Path;
     }
 }
