4d60f7589109ced0c5ea0f07ed7734195eb710aa
diligite-iustitiam <107812456+diligite-iustitiam@users.noreply.github.com>
2023-01-09 22:21:51 +0200

    CryptoCurrency model created

---
+++ CoinGeckoWPFTestProject/Models/CryptoCurrencies.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinGeckoWPFTestProject.Models
+{
+    internal class CryptoCurrencies
+    {
+        public string? CurrencyID { get; set; }
+
+        public string? CurrencyName { get; set; }
+
+        public decimal CurrentPrice { get; set; }
+
+        public double PriceChange { get; set; }
+
+        public decimal TotalVolume { get; set; }
+
+        public double MarketCap { get; set; }
+
+        public int MarketCapRank { get; set; }
+
+        public DateTime? LastUpdate { get; set; }
+
+    }
+}
---
+++ CoinGeckoWPFTestProject/Services/CurrencyService.cs
@@ -0,0 +1,18 @@
+﻿using CoinGeckoWPFTestProject.Models;
+using CoinGeckoWPFTestProject.Services.Intefraces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinGeckoWPFTestProject.Services
+{
+    internal class CurrencyService : ICurrencyService
+    {
+        public Task<CryptoCurrencies> GetCryptoCurrencyID()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
